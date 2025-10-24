1d4fa4f28eace18657b30813552538e2757e6363
motuptlu <motupatlu8875@gmail.com>
2025-10-24 23:04:36 +0530

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Anovas | Web & App Development, Designing & Branding Agency - Boost Sales & Conversions at light speed" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
